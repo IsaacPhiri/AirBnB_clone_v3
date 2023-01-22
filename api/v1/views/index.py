ab43dd205f0d3395e7f85a0c2881901d88d52025
Isaac Phiri <isaacphiri315@gmail.com>
2023-01-22 03:48:06 +0000

    Api

---
+++ api/v1/views/index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route("/status", methods=['GET'])
+def status():
+    """ Return the status of the API """
+    return jsonify(status="OK")
